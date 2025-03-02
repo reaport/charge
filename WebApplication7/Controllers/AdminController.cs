e2ca83a2c90fa1b1fad8d3bbe1038249ccdb6289
JulKhizhn <muskoshka@mail.ru>
2025-03-02 19:50:10 +0300

    комментарии

--- WebApplication7/Controllers/AdminController.cs
+++ WebApplication7/Controllers/AdminController.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 
 namespace ChargeModule.Controllers
 {
+    // Контроллер для администрирования системы зарядки
     [Route("charge/admin")]
     public class AdminController : Controller
     {
@@ -15,6 +16,7 @@ namespace ChargeModule.Controllers
         private readonly IChargeService _chargeService;
         private readonly ILogger<AdminController> _logger;
 
+        // Конструктор контроллера, принимает зависимости через DI
         public AdminController(IAdminConfigService adminConfigService, IChargeService chargeService, ILogger<AdminController> logger)
         {
             _adminConfigService = adminConfigService;
@@ -22,6 +24,7 @@ namespace ChargeModule.Controllers
             _logger = logger;
         }
 
+        // Отображает страницу админки (GET /charge/admin)
         [HttpGet]
         public IActionResult Index()
         {
@@ -33,12 +36,15 @@ namespace ChargeModule.Controllers
             return View(model);
         }
 
+        // Обновляет параметры конфигурации (POST /charge/admin/update)
         [HttpPost("update")]
         public async Task<IActionResult> Update()
         {
+            // Логируем тело запроса
             string requestBody = await ReadRequestBody();
             _logger.LogInformation("Запрос обновления конфигурации:\n{RequestBody}", requestBody);
 
+            // Обновляем конфигурацию
             var config = new AdminConfig
             {
                 MovementSpeed = double.Parse(Request.Form["MovementSpeed"]),
@@ -50,12 +56,15 @@ namespace ChargeModule.Controllers
             return RedirectToAction("Index");
         }
 
+        // Запрос на регистрацию новой машины (POST /charge/admin/registerVehicle)
         [HttpPost("registerVehicle")]
         public async Task<IActionResult> RegisterVehicle()
         {
+            // Логируем тело запроса
             string requestBody = await ReadRequestBody();
             _logger.LogInformation("Запрос регистрации транспортного средства:\n{RequestBody}", requestBody);
 
+            // Извлекаем тип машины из формы
             string type = Request.Form["Type"];
             await _chargeService.RegisterVehicleAsync(type);
             _logger.LogInformation("Запрос на регистрацию транспортного средства типа {Type} успешно обработан.", type);
@@ -63,6 +72,7 @@ namespace ChargeModule.Controllers
             return RedirectToAction("Index");
         }
 
+        // Метод для чтения тела запроса
         private async Task<string> ReadRequestBody()
         {
             Request.EnableBuffering();
--- WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
+++ WebApplication7/obj/Debug/net6.0/WebApplication7.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+714be022f56f25e095900a05fa7154a50f195f16")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f633eab9d1cb1668850e90c5eea4c27f9cfda28b")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApplication7")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
